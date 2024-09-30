96ead07034f71c951a557b03d107779f3f36cea9
Bravestone01 <mujtabashah906@gmail.com>
2024-09-30 17:28:25 +0500

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { useEffect, useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [count,setCount]=useState(0)
+  const[data,setData]=useState(100)
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View >
+      <Text style={styles.container}>useEffect as component did update</Text>
+      
+      <User info={{data,count}}/>
+      <View style={{marginBottom:80}}></View>
+  
+      <Button title='update Counter' onPress={()=>setCount(count+1)}/>
+        <View style={{marginTop:10,}}></View>
+      <Button title='update data' onPress={()=>setData(data+1)}/>
+      <View style={{marginTop:10,}}></View>
+
+      <Button title='reset' onPress={()=>{ setData(100), setCount(0),console.warn("everything is reset")}}/>
+        
+       
+      
     </View>
   );
 }
 
+const User=(props)=>{
+
+  useEffect(()=>{
+
+    console.warn("data are updated");
+    
+  },[props.info.data])
+  useEffect(()=>{
+
+    console.warn("count are updated");
+    
+  },[props.info.count])
+
+  return (
+    <View >
+      <Text style={styles.txt}>Count : {props.info.count}</Text>
+      <Text style={styles.txt}>Data : {props.info.data}</Text>
+     
+      
+    </View>
+  );
+
+}
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+   marginTop:20,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    fontSize:30,
+    backgroundColor:"red",
+    padding:10,
+    color:"white",
+    marginBottom:100,
+   
   },
+  txt:{
+    fontSize:30,
+    backgroundColor:"black",
+    color:"white",
+    textAlign:"center",
+    padding:40,
+    margin:20
+  }
 });
